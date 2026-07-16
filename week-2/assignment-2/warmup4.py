b440d4aeb1f9ed83ac9e05b4d6b015e09d348643
samm-e13 <navigatorddg54.com>
2026-07-15 22:20:36 -0400

    made formatting fixes

--- week-2/assignment-2/warmup4.py
+++ week-2/assignment-2/warmup4.py
@@ -1,11 +1,11 @@
-# 1. What the error message said: Traceback (most recent call last):
+print("""# 1. What the error message said: Traceback (most recent call last):
   #File "/Users/royortiz/my_project/python_class/working/roy-ortiz-python/python-intro-homework/week-2/assignment-2/warmup4.py", line 1, in <module>
    # age = forty-three
           ^^^^^
-#NameError: name 'forty' is not defined
+#NameError: name 'forty' is not defined""")
 
-#2. What caused it: I used text data-type instead of the intended integer.
-#3. I fixed by changing the var definition from text to integer.
+print("#2. What caused it: I used text data-type instead of the intended integer.")
+print("#3. I fixed by changing the var definition from text to integer.")
 
-age = forty-three
+age = 40
 print(age)
