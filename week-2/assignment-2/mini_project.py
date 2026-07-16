a67c4f3d378b26550793c3505492bf3dfb500724
samm-e13 <navigatorddg54.com>
2026-07-15 22:39:56 -0400

    formatting changes

--- week-2/assignment-2/mini_project.py
+++ week-2/assignment-2/mini_project.py
@@ -1,4 +1,3 @@
-f = float(input("Enter a temperature in Farenheit: "))
+f = float(input("Enter a temperature in Fahrenheit: "))
 c = round(((f - 32) * 5)/9, 1)
-print(f"Enter a temperature in Farenheit: {f}")
 print(f"{f:.1f}\N{DEGREE SIGN}F is {c:.1f}\N{DEGREE SIGN}C")
