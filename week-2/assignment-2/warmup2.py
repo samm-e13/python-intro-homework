cd3ecade1422f071aece819d3655917612956ea6
samm-e13 <navigatorddg54.com>
2026-07-15 22:12:41 -0400

    Fixed formatting

--- week-2/assignment-2/warmup2.py
+++ week-2/assignment-2/warmup2.py
@@ -1,8 +1,7 @@
-#Navigation commands I used: 
-#pwd /Users/royortiz
-#cd /Users/royortiz/my_project/python_class/working/roy-ortiz-python/python-intro-homework
-#cd week-2/assignment-2
-#pwd /Users/royortiz/my_project/python_class/working/roy-ortiz-python/python-intro-homework/week-2/assignment-2
+print("#Navigation commands I used:")
+print("#pwd /Users/royortiz")
+print("#pwd /Users/royortiz/my_project/python_class/working/roy-ortiz-python/python-intro-homework/week-2/assignment-2")
+print("#ls")
 
-date = input("What is today's date?")
+date = input("What is today's date? ")
 print(f"You said today is {date}.")
