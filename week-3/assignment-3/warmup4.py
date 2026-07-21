ca14c34a81dd51533c0eb699d67cb219d814b343
samm-e13 <navigatorddg54.com>
2026-07-21 08:13:47 -0400

    Another attempt to align warmup4 to the instructions

--- week-3/assignment-3/warmup4.py
+++ week-3/assignment-3/warmup4.py
@@ -1,11 +1,11 @@
 num = int(input("Enter a number: "))
 
+if num == 0:
+    print(f"{num} is zero.")
 if num > 0:
     print(f"{num} is positive.")
-elif num < 0:
-    print(f"{num} is negative.")
 else:
-    print(f"{num} is zero.")
+    print(f"{num} is negative.")
 
 if num % 2 == 0:
     print(f"{num} is even.")
