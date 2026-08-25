8535b27a28a4cad917b3ec24fcdc8f1ee3d6479b
samm-e13 <navigatorddg54.com>
2026-08-24 23:46:32 -0400

    third fix

--- week-8/assignment-8/warmup2.py
+++ week-8/assignment-8/warmup2.py
@@ -3,9 +3,9 @@ denominator = input(f"Enter the denominator:")
 
 try:
     result = (float(numerator) / float(denominator))
-    print(f"{numerator} \u00f7 {denominator} = {result:.1f}")
+    print(f"{float(numerator)} \u00f7 {float(denominator)} = {result:.1f}")
 except ZeroDivisionError:
-    print(f"Can't divide by zero - please try a non-zero denominator.")
+    print(f"Can't divide by zero \u2014 please try a non-zero denominator.")
 
 
 
